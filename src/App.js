6fb2c92630b94eec8356f858abc2e2bca86de06b
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-29 23:08:05 +0500

    Added routes for:
    * Home
    * Login
    * Signup
    * Order

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Burger from "./components/Burger";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Order from "./components/Order";
 
 function App() {
   return (
-    <div className="burger_container">
-      <Burger />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div className="burger-container">
+          <Switch>
+            <Route exact path="/">
+              <Burger />
+            </Route>
+            <Route path="/auth/login">
+              <Login />
+            </Route>
+            <Route path="/auth/signup">
+              <Register />
+            </Route>
+            <Route path="/order">
+              <Order />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 export default App;
