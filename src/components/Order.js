01b1a28e7f7c75220b4f4560e3ab9c09e64e79c0
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-29 23:19:17 +0500

    Added Order component to display tha final order

---
+++ src/components/Order.js
@@ -0,0 +1,46 @@
+import { Button } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import "./Burger.css";
+
+function Order() {
+  let location = useLocation();
+  const burger = location.state.burger;
+
+  const displayOrder = () => {
+    let burgers = [];
+
+    for (let i = 0; i < burger.lettuce; i++) {
+      burgers.push(<div key={burgers.length} className="lettuce"></div>);
+    }
+    for (let i = 0; i < burger.bacon; i++) {
+      burgers.push(<div key={burgers.length} className="bacon"></div>);
+    }
+    for (let i = 0; i < burger.cheese; i++) {
+      burgers.push(<div key={burgers.length} className="cheese"></div>);
+    }
+    for (let i = 0; i < burger.meat; i++) {
+      burgers.push(<div key={burgers.length} className="meat"></div>);
+    }
+    if (burgers.length === 0) {
+      return <h2>It won't taste good</h2>;
+    }
+    return burgers;
+  };
+  return (
+    <>
+      <h2>We hope it tastes well!</h2>
+      <div className="burgerIngredients">
+        <div className="top"></div>
+        {displayOrder()}
+        <div className="bottom"></div>
+      </div>
+      <div className="ingredientBtns">
+        <Button className="btn btn-primary">Continue</Button>
+        <Link to={"/"}>
+        <Button className="btn-danger">Cancel</Button>
+        </Link>
+      </div>
+    </>
+  );
+}
+export default Order;
