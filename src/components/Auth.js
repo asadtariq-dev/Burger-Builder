eb4f0be5034280a0408701ad3d02eefb605dbdab
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-03 01:39:42 +0500

    Modified the code made it dry

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,7 +4,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { auth } from "../firebase-config";
+import { auth } from "../database/firebase-config";
 import { Link } from "react-router-dom";
 import "./Auth.css";
 import Loader from "./Loader";
@@ -21,16 +21,13 @@ function Auth() {
     try {
       setLoader(true);
       if (flag === "login") {
-        await signInWithEmailAndPassword(auth, email, password).then(() => {
-          setLoader(false);
-          navigate("/");
-        });
+        await signInWithEmailAndPassword(auth, email, password);
+        setLoader(false);
+        navigate("/");
       } else if (flag === "register") {
-        await createUserWithEmailAndPassword(auth, email, password).then(() => {
-          setLoader(false);
-          alert("Account Created");
-        });
-
+        await createUserWithEmailAndPassword(auth, email, password);
+        setLoader(false);
+        alert("Account Created");
         setError("");
       }
     } catch (error) {
@@ -39,72 +36,65 @@ function Auth() {
     }
   };
 
+  if (loader) {
+    return <Loader />;
+  }
   return (
     <div className="Auth-form-container">
-      {loader === false ? (
-        <div className="Auth-form">
-          {flag === "login" ? (
-            <h2>Login here</h2>
-          ) : (
-            <h2>Register your account</h2>
-          )}
-          <div className="Auth-form-content">
-            <div className="form-group mt-3">
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-              />
-            </div>
-            {error ? (
-              <p className="text-center text-danger mt-2">{error}</p>
-            ) : null}
-            <div className="d-grid gap-2 mt-3">
-              <button onClick={onSubmit} className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <p className="d-grid gap-2 mt-3">
-              {flag === "login" ? (
-                <Link
-                  className="btn btn-secondary"
-                  onClick={(event) => {
-                    setFlag("register");
-                  }}
-                  to={"/auth"}
-                >
-                  Register
-                </Link>
-              ) : (
-                <Link
-                  className="btn btn-secondary"
-                  onClick={(event) => {
-                    setFlag("login");
-                  }}
-                  to={"/auth"}
-                >
-                  Login
-                </Link>
-              )}
-            </p>
+      <div className="Auth-form">
+        {flag === "login" ? (
+          <h2>Login here</h2>
+        ) : (
+          <h2>Register your account</h2>
+        )}
+        <div className="Auth-form-content">
+          <div className="form-group mt-3">
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
           </div>
+          <div className="form-group mt-3">
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+          </div>
+          {error && <p className="text-center text-danger mt-2">{error}</p>}
+          <div className="d-grid gap-2 mt-3">
+            <button onClick={onSubmit} className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="d-grid gap-2 mt-3">
+            {flag === "login" ? (
+              <Link
+                className="btn btn-secondary"
+                onClick={() => setFlag("register")}
+                to={"/auth"}
+              >
+                Register
+              </Link>
+            ) : (
+              <Link
+                className="btn btn-secondary"
+                onClick={() => setFlag("login")}
+                to={"/auth"}
+              >
+                Login
+              </Link>
+            )}
+          </p>
         </div>
-      ) : (
-        <Loader />
-      )}
+      </div>
     </div>
   );
 }
